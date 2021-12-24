e16f7c02f286c1c7f46aaee207c81102bb909028
igornfaustino <igor@nfaustino.com>
2021-12-24 01:28:28 -0300

    ensure calls log function with right params

--- src/domain/__tests__/entities/ProtectedEntryProxy.spec.ts
+++ src/domain/__tests__/entities/ProtectedEntryProxy.spec.ts
@@ -7,8 +7,14 @@ import { SaveAccessLogRepository } from '../../interfaces/SaveAccessLogRepositor
 class SaveAccessLogRepositoryMock implements SaveAccessLogRepository {
 	calls = 0;
 
+	params?: {
+		user: IUser;
+		entry: IEntry;
+	};
+
 	log(user: IUser, entry: IEntry): Promise<void> {
 		this.calls += 1;
+		this.params = { user, entry };
 
 		return Promise.resolve();
 	}
@@ -16,20 +22,21 @@ class SaveAccessLogRepositoryMock implements SaveAccessLogRepository {
 
 describe('ProtectedEntryProxy', () => {
 	const entryValue = 'protected value';
+	const user: IUser = {
+		email: 'test@test.com',
+		name: 'john do',
+		id: '1234',
+	};
 
 	const makeSut = () => {
 		const saveAccessLogRepository = new SaveAccessLogRepositoryMock();
-		const user: IUser = {
-			email: 'test@test.com',
-			name: 'john do',
-			id: '1234',
-		};
 		const entry = new Entry(entryValue);
 		const sut = new ProtectedEntryProxy(entry, saveAccessLogRepository, user);
 
 		return {
 			sut,
 			saveAccessLogRepository,
+			entry,
 		};
 	};
 
@@ -42,11 +49,13 @@ describe('ProtectedEntryProxy', () => {
 	});
 
 	it('Should log value when user tries to get data', async () => {
-		const { sut, saveAccessLogRepository } = makeSut();
+		const { sut, saveAccessLogRepository, entry } = makeSut();
 
 		const value = await sut.getEntryValue();
 
 		expect(value).toBe(entryValue);
 		expect(saveAccessLogRepository.calls).toBe(1);
+		expect(saveAccessLogRepository.params?.user).toBe(user);
+		expect(saveAccessLogRepository.params?.entry).toBe(entry);
 	});
 });
