dde163a5086b9ae5f772922a6b101c4f92f8dd11
igornfaustino <igor@nfaustino.com>
2021-12-24 01:10:08 -0300

    extract test setup into a factory

--- src/domain/__tests__/entities/ProtectedEntryProxy.spec.ts
+++ src/domain/__tests__/entities/ProtectedEntryProxy.spec.ts
@@ -2,10 +2,20 @@ import { Entry } from '../../entities/implementations/Entry';
 import { ProtectedEntryProxy } from '../../entities/implementations/ProtectedEntryProxy';
 
 describe('ProtectedEntryProxy', () => {
-	test('Should log user data when get entry data', () => {
-		const entry = new Entry('protected value');
+	const entryValue = 'protected value';
+
+	const makeSut = () => {
+		const entry = new Entry(entryValue);
 		const sut = new ProtectedEntryProxy(entry);
 
-		expect(sut.getEntryValue()).toBe('protected value');
+		return {
+			sut,
+		};
+	};
+
+	it('Should get right value from entry', () => {
+		const { sut } = makeSut();
+
+		expect(sut.getEntryValue()).toBe(entryValue);
 	});
 });
